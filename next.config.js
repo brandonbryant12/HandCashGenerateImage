c450a25b12ca9927cd3f195a3a500e8b0a022cc5
34001956 <apascualagut@gmail.com>
2022-12-15 00:39:42 +0100

    Updated config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['cloud.handcash.io', 'res.cloudinary.com'],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
