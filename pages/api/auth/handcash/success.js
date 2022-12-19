a3a5fb2bed24e407dc254758c51f1310a350eeb8
34001956 <apascualagut@gmail.com>
2022-12-19 21:55:42 +0100

    updated success

--- pages/api/auth/handcash/success.js
+++ pages/api/auth/handcash/success.js
@@ -1,22 +1,21 @@
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import AuthTokenRepository from "../../../../src/repositories/AuthTokenRepository";
 import HandCashService from "../../../../src/services/HandCashService";
 import SessionTokenRepository from "../../../../src/repositories/SessionTokenRepository";
 import ProfileRepository from "../../../../src/repositories/ProfileRepository";
 
-
 export default async function handler(req, res) {
-    const {authToken} = req.query;
+  const { authToken } = req.query;
 
-    const {publicProfile} = await new HandCashService(authToken).getProfile();
+  const { publicProfile } = await new HandCashService(authToken).getProfile();
 
-    const user = await ProfileRepository.add(publicProfile)
+  const user = await ProfileRepository.add(publicProfile);
 
-    const payload = {
-        sessionId: uuidv4(),
-        user,
-    };
-    const sessionToken = SessionTokenRepository.generate(payload);
-    AuthTokenRepository.setAuthToken(authToken, payload.sessionId);
-    return res.redirect(`/?sessionToken=${sessionToken}`);
+  const payload = {
+    sessionId: uuidv4(),
+    user,
+  };
+  const sessionToken = SessionTokenRepository.generate(payload);
+  AuthTokenRepository.setAuthToken(authToken, payload.sessionId);
+  return res.redirect(`/?sessionToken=${sessionToken}`);
 }
