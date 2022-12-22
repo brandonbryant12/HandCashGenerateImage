e27e6a5e8b796f90131facffff4aa044b377ae62
34001956 <apascualagut@gmail.com>
2022-12-22 03:22:49 +0100

    removed unused config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,7 @@ module.exports = {
     ],
     theme: {
       extend: {
-        fontFamily: {
-          Figtree: ["Figtree", "sans-serif"],
-        },
         colors: {
-          brandGreen: "#41FB74",
-          brandDarkGreen: "#3EC163",
-          brandBlue: "#084C70",
-          brandDarkBlue: "#031A28",
           twitterBlue: "#1DA1F2",
           twitterDarkBlue: "#1077AC",
         },
